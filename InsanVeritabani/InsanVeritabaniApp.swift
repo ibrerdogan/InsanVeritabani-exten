67463c9154136cbaace47ce0fc64cc9f33ca7f0f
ibrerdogan <94644110+ibrerdogan@users.noreply.github.com>
2022-07-25 12:55:04 +0300

    Initial Commit

---
+++ InsanVeritabani/InsanVeritabaniApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsanVeritabaniApp.swift
+//  InsanVeritabani
+//
+//  Created by İbrahim Erdogan on 25.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct InsanVeritabaniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
